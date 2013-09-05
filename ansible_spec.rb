f0203deffb5fe56b0dde833dee1412e1d9fab6ed
Charles Pence <charles@charlespence.net>
2013-09-05 13:26:49 -0400

    Add Ansible support for deploying Tomcat, specs.

--- ansible_spec.rb
+++ ansible_spec.rb
@@ -61,6 +61,36 @@ describe 'Ansible provisioning', :deploy do
     end
   end
 
+  describe 'solr role' do
+    describe 'tomcat.yml' do
+      it 'starts the Tomcat server' do
+        expect(vagrant_ssh('/etc/init.d/tomcat status')).to include('Tomcat is running')
+      end
+
+      it 'makes Tomcat functional' do
+        expect(vagrant_ssh('wget -q -O- http://localhost:8080')).to include('<title>Apache Tomcat/7')
+      end
+
+      it 'creates the Tomcat password file' do
+        file_path = Rails.root.join('deploy', 'roles', 'solr', 'files', 'tomcat_192.168.111.222_pass')
+        expect(File.exist?(file_path)).to be_true
+      end
+
+      it 'configures the Tomcat administration GUI' do
+        file_path = Rails.root.join('deploy', 'roles', 'solr', 'files', 'tomcat_192.168.111.222_pass')
+        tomcat_password = IO.read(file_path).sub("\n", '')
+
+        expect(vagrant_ssh("wget -q -O- http://rletters_tomcat:#{tomcat_password}@localhost:8080/manager/html")).to include('<title>/manager</title>')
+      end
+
+      it 'does not make the Tomcat server publically accessible' do
+        # Since the web server and Solr server are both on the same host, the
+        # scripts should *not* open port 8080 through iptables.
+        vagrant_check_port_closed(8080)
+      end
+    end
+  end
+
   describe 'web role' do
     describe 'ruby.yml' do
       it 'successfully installs ruby' do
